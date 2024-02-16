bb2c8f7ee3928730df89fdf701b19dd6ccb2abd3
ajaykrmnc <ajaykg6917@gmail.com>
2024-02-16 20:29:00 +0530

    added

---
+++ abstract-classes/virtual.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+class Base{
+public:
+    void sayHello(){
+        cout << "This is Base Class" << endl;
+    }
+    virtual void virtualSayHello(){
+        cout << "This is Base class" << endl;
+    }
+};
+class Child : public Base {
+public:
+    void virtualSayHello(){
+        cout << "This is child class" << endl;
+    }
+    void sayHello(){
+        cout << "This is child class" << endl;
+    }
+};
+int main(){
+    Base *baseClassPtr;
+    Child childClass;
+    baseClassPtr = &childClass;
+    baseClassPtr->sayHello();
+    baseClassPtr->virtualSayHello();
+}
